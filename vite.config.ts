c53a1076696a5d5de714d32542e3167f71a69f84
Tobias Eriksson <toberi@outlook.com>
2023-11-07 03:35:48 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     plugins: [react()],
 
     build: {
-        outDir: 'dist',
+        outDir: 'build',
         rollupOptions: {
             input: './src/main.tsx'
         }
